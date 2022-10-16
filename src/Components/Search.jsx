fca1e964c89c903829330e30f1ef9cee7ac028b3
Eli Guy <eliguy343@gmail.com>
2022-10-16 20:12:18 +0300

    Find a user

--- src/Components/Search.jsx
+++ src/Components/Search.jsx
@@ -1,6 +1,32 @@
-import { Avatar, Box, TextField, Typography } from '@mui/material';
-
+import { Avatar, Box, formGroupClasses, TextField, Typography } from '@mui/material';
+import { useState } from 'react';
+import {collection, getDocs, query, where} from "firebase/firestore";
+import { db } from '../firebase';
 const Search = () => {
+  const [username, setUsername] = useState();
+  const [user, setUser] = useState();
+  const [err, setErr] = useState();
+
+  const handeSearch = async () => {
+    const q = query(
+      collection(db, "users"),
+      where("displayName", "==", username
+    ));
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    }
+    catch (err) {
+      console.log(err);
+      setErr(err);
+    }
+  }
+  const handleKey = (e) => {
+    e.code === "Enter" && handeSearch();
+  }
+
   return (
     <Box
       sx={{
@@ -21,9 +47,12 @@ const Search = () => {
             color:'white'
           }
         }}
+        onChange={(e) => setUsername(e.target.value)}
+        onKeyDown={handleKey}
       />
       </Box>
-      <Box
+      {err && <Typography>User not Found</Typography>}
+      {user && <Box
         sx={{
           padding:'5px',
           display:'flex',
@@ -34,16 +63,16 @@ const Search = () => {
           }
         }}
       >
-        <Avatar alt='Kyle Katarn' src='/static/Kyle.jpg'/>
+        <Avatar alt={user.displayName} src={user.photoURL}/>
         <Typography
           sx={{
             fontSize:'15px',
             fontWeight:'600'
           }}
         >
-          Kyle Katarn
+          {user.displayName}
         </Typography>
-      </Box>
+      </Box>}
     </Box>
   )
 }
