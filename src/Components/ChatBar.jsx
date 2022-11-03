6f2709a52781b808141bb798ffc1b9399c8e7e27
Eli Guy <eliguy343@gmail.com>
2022-11-03 17:38:36 +0200

    cleaned up chatbar

--- src/Components/ChatBar.jsx
+++ src/Components/ChatBar.jsx
@@ -3,21 +3,11 @@ import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import PersonAddIcon from '@mui/icons-material/PersonAdd';
-import VideoCallIcon from '@mui/icons-material/VideoCall';
-import MenuIcon from '@mui/icons-material/Menu';
-import { Button, IconButton, Menu, MenuItem, Tooltip } from '@mui/material';
+import { Avatar } from '@mui/material';
 import { useSelector } from 'react-redux';
 
 export default function ButtonAppBar() {
   const user = useSelector(state=> state.chat.user);
-  const [anchorElUser, setAnchorElUser] = React.useState(null);
-  const handleOpenUserMenu = (event) => {
-    setAnchorElUser(event.target);
-  }
-  const handleCloseUserMenu = () => {
-    setAnchorElUser(false);
-  }
   return (
     <Box>
       <AppBar
@@ -30,10 +20,15 @@ export default function ButtonAppBar() {
         <Toolbar
           sx={{
             display:'flex',
-            justifyContent:'space-between'
+            justifyContent:'start',
+            gap:'5px'
           }}
         >
           {user.uid && <>
+            <Avatar
+              alt={user.name}
+              src={user.photoURL}
+            />
             <Typography
               sx={{
                 fontsize:'30px',
@@ -42,65 +37,6 @@ export default function ButtonAppBar() {
             >
               {user.displayName}
             </Typography>
-            <Box
-              sx={{
-                display:'flex',
-                alignItems:'center',
-              }}
-            >
-              <Button
-                sx={{
-                  color: 'white',
-                  p:0
-                }}
-              >
-                <PersonAddIcon/>
-              </Button>
-              <Button
-                sx={{
-                  color: 'white',
-                  p:0
-                }}
-              >
-                <VideoCallIcon/>
-              </Button>
-              <Tooltip title="Open settings">
-                <IconButton
-                  onClick={handleOpenUserMenu}
-                  sx={{ p: 0 }}
-                  aria-label="account of current user"
-                  aria-controls="menu-appbar"
-                  aria-haspopup="true"
-                >
-                  <MenuIcon
-                    sx={{
-                      color:'white',
-                      marginLeft:'5px'
-                    }}
-                  />
-                </IconButton>
-              </Tooltip>
-              <Menu
-                sx={{ mt: '45px' }}
-                id="menu-appbar"
-                anchorEl={anchorElUser}
-                anchorOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                keepMounted
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-                open={Boolean(anchorElUser)}
-                onClose={handleCloseUserMenu}
-              >
-                  <MenuItem onClick={handleCloseUserMenu}>
-                    <Typography textAlign="center">Test</Typography>
-                  </MenuItem>
-              </Menu>
-            </Box>
           </>}
         </Toolbar>
       </AppBar>
